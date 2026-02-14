d694d95f80c3d59dacf145758945dad48d5fa4b8
mohammadmaleh <mohammad.maleh@gmail.com>
2026-02-14 09:49:20 +0100

    feat: add Label primitive component for form inputs

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -7,3 +7,4 @@ export type { ButtonProps } from "./components/primitives/button";
 export { Input } from "./components/primitives/input";
 export type { InputProps } from "./components/primitives/input";
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./components/primitives/card";
+export { Label } from "./components/primitives/label";
